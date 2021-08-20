b20924b6940ee3e08a40dd22ebe7737aca327e5a
Chris Beck <cbeck@hashicorp>
2021-08-20 10:33:05 -0400

    update vpc

--- tfcode/vpc.tf
+++ tfcode/vpc.tf
@@ -1,15 +1,16 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
+  tags = {
     Owner = "cbeck"
+    Name  = "cbeck-vpc"
   }
 }
 
 output "vpcid" {
-    value = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
